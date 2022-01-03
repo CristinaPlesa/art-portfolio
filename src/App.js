4899daeece6e7ae56420f675e80d36546d520caf
Cristina Plesa <cristinaplesa8@gmail.com>
2022-01-03 14:50:39 -0800

    add component files and home about section
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Header from './Components/Header/Header.js'
+// import Header from './Components/Header/Header.js'
+import Home from './Components/Home/Home.js'
  
 function App() {
   return (
     <div className="App">
-    <Header />
+    <Home />
     </div>
   );
 }
